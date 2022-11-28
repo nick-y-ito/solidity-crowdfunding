4af0b1439039dbf5a9776e3099d73fe2c7ea5d01
Yusuke Ito <u.skayyyyy@gmail.com>
2022-11-28 22:06:39 +0900

    Fix factory instance defining as const

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,11 +1,9 @@
 import web3 from "./web3.js";
 import compiledFactory from "../ethereum/build/CampaignFactory.json" assert { type: "json" };
 
-function factory() {
-	return new web3.eth.Contract(
-		compiledFactory.abi,
-		process.env.NEXT_PUBLIC_FACTORY_ADDRESS
-	);
-}
+const factory = new web3.eth.Contract(
+	compiledFactory.abi,
+	process.env.NEXT_PUBLIC_FACTORY_ADDRESS
+);
 
 export default factory;
